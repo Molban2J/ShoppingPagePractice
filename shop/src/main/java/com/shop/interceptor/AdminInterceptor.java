c7124de97d1a7b5b25e9465758fec7295f4b6ff1
molban2j <jjimmee1@gmail.com>
2023-05-10 15:22:33 +0900

    interpretor add

--- shop/src/main/java/com/shop/interceptor/AdminInterceptor.java
+++ shop/src/main/java/com/shop/interceptor/AdminInterceptor.java
@@ -1,5 +1,24 @@
 package com.shop.interceptor;
 
-public class AdminInterceptor {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import com.shop.model.MemberVO;
+
+public class AdminInterceptor implements HandlerInterceptor{
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		HttpSession session = request.getSession();
+		MemberVO lvo = (MemberVO)session.getAttribute("member");
+		if(lvo == null || lvo.getAdminCk() == 0) {  //로그인 상태가 아니거나 관리자 계정이 아닌경우
+			response.sendRedirect("/main");		// 메인페이지로 리다이렉트
+			return false;
+		}
+		
+		return true;
+	}
 }
